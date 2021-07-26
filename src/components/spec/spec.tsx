e156cc99c18d89e2d44f72683557599d0e9b0677
Marat <na-journal@yandex.ru>
2021-07-27 01:37:19 +0400

    F-04 Добавил вывод стоимости выбранного авто

--- src/components/spec/spec.tsx
+++ src/components/spec/spec.tsx
@@ -3,13 +3,15 @@ import { useDispatch, useSelector } from 'react-redux';
 
 import { Button } from '@components/common/button';
 import { SpecItem } from '@components/spec/spec-item';
-import { getActiveTabIndex, getOrder, getTabs } from '@state/selectors';
+import { getActiveTabIndex, getCar, getOrder, getTabs } from '@state/selectors';
 import { TabActiveAction } from '@state/tabs/actions';
+import { numSpace } from '@utils/num-space';
 
 import './spec.scss';
 
 export const Spec: FC = () => {
     const dispatch = useDispatch();
+    const car = useSelector(getCar);
     const tabs = useSelector(getTabs);
     const order = useSelector(getOrder);
     const activeTabIndex = useSelector(getActiveTabIndex);
@@ -30,9 +32,14 @@ export const Spec: FC = () => {
                         />
                     ))}
             </div>
-            <div className='spec__cost'>
-                <span>Цена:</span> от 8 000 до 12 000 ₽
-            </div>
+            {car.value ? (
+                <div className='spec__cost'>
+                    <span>Цена:</span>
+                    {` от ${numSpace(car.value.priceMin)} до ${numSpace(
+                        car.value.priceMax
+                    )} ₽`}
+                </div>
+            ) : null}
             <div className='spec__next'>
                 <Button
                     disabled={!tabs[activeTabIndex].complete}
