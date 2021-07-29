2b93b36eddf5bc3e1cc1bbe58e1e835f08aa0679
marat_khas <na-journal@yandex.ru>
2021-07-29 18:56:07 +0400

    F-05 Добавил расчет стоимости

--- src/components/spec/spec.tsx
+++ src/components/spec/spec.tsx
@@ -1,10 +1,18 @@
-import { FC } from 'react';
+import { FC, useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 
 import { Button } from '@components/common/button';
 import { SpecItem } from '@components/spec/spec-item';
 import { Cart } from '@state/order/types';
-import { getActiveTabIndex, getCar, getOrder, getTabs } from '@state/selectors';
+import {
+    getActiveTabIndex,
+    getAdditionals,
+    getCar,
+    getDate,
+    getOrder,
+    getRate,
+    getTabs,
+} from '@state/selectors';
 import { TabActiveAction } from '@state/tabs/actions';
 import { numSpace } from '@utils/num-space';
 
@@ -16,9 +24,71 @@ export const Spec: FC = () => {
     const tabs = useSelector(getTabs);
     const order = useSelector(getOrder);
     const activeTabIndex = useSelector(getActiveTabIndex);
+
     const handleNext = () => {
         dispatch(TabActiveAction(activeTabIndex + 1));
     };
+
+    const selectedDates = useSelector(getDate);
+    const selectedRate = useSelector(getRate);
+    const additionals = useSelector(getAdditionals);
+
+    const [cost, setCost] = useState<string | null>(null);
+
+    useEffect(() => {
+        const additionCost = additionals.reduce(
+            (acc, cur) => (cur.selected ? acc + cur.cost : acc),
+            0
+        );
+
+        if (selectedDates.from && selectedDates.to && selectedRate.value) {
+            const time = Math.ceil(
+                (selectedDates.to.getTime() - selectedDates.from.getTime()) /
+                    (1000 * 60)
+            );
+            switch (selectedRate.value.rateTypeId.unit) {
+                case 'мин':
+                    setCost(
+                        `${numSpace(
+                            selectedRate.value.price * time + additionCost
+                        )} ₽`
+                    );
+                    break;
+                case '7 дней':
+                    setCost(
+                        `${numSpace(
+                            selectedRate.value.price *
+                                Math.ceil(time / (60 * 24 * 7)) +
+                                additionCost
+                        )} ₽`
+                    );
+                    break;
+                default:
+                    setCost(
+                        `${numSpace(
+                            selectedRate.value.price *
+                                Math.ceil(time / (60 * 24)) +
+                                additionCost
+                        )} ₽`
+                    );
+            }
+        } else if (car.value) {
+            setCost(
+                ` от ${numSpace(car.value.priceMin)} до ${numSpace(
+                    car.value.priceMax
+                )} ₽`
+            );
+        } else {
+            setCost(null);
+        }
+    }, [
+        selectedDates.from,
+        selectedDates.to,
+        selectedRate.value,
+        additionals,
+        car.value,
+    ]);
+
     return (
         <div className='spec'>
             <div className='spec__head'>Ваш заказ:</div>
@@ -39,12 +109,9 @@ export const Spec: FC = () => {
                     ) : null
                 )}
             </div>
-            {car.value ? (
+            {cost ? (
                 <div className='spec__cost'>
-                    <span>Цена:</span>
-                    {` от ${numSpace(car.value.priceMin)} до ${numSpace(
-                        car.value.priceMax
-                    )} ₽`}
+                    <span>Цена:</span> {cost}
                 </div>
             ) : null}
             <div className='spec__next'>
